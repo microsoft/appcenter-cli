86e6cc6307ff29ac8e18ef782f71f587da53395d
Chris Tavares <ctavares@microsoft.com>
2016-09-17 23:03:51 -0700

    Hooking up tests in package.json
    Added support modules for running typescript tests and cleaning up
    after build.

---
+++ test/bootstrap.ts
@@ -0,0 +1,13 @@
+// Typescript test to verify that mocha/chai actually works
+
+import { expect } from "chai";
+
+describe("bootstrap", function () {
+    it("should run this", () => {
+        expect(true).to.be.true;
+    });
+
+    it("should fail this", () => {
+        expect(1).to.equal(2);
+    });
+});
