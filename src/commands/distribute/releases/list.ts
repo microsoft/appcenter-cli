e7b3dc60b804253270137f2cdd4e131138051440
Dmitry Bryukov <v-dmbryu@microsoft.com>
2017-03-29 22:33:17 +0300

    Implemented 'distribute releases list' command

---
+++ src/commands/distribute/releases/list.ts
@@ -0,0 +1,36 @@
+import { AppCommand, CommandResult, ErrorCodes, failure, help, success } from "../../../util/commandline";
+import { MobileCenterClient, models, clientRequest } from "../../../util/apis";
+import { out } from "../../../util/interaction";
+import { inspect } from "util";
+import * as _ from "lodash";
+
+const debug = require("debug")("mobile-center-cli:commands:distribute:releases:list");
+
+@help("Shows the list of all releases for the application")
+export default class ShowReleasesCommand extends AppCommand {
+
+  public async run(client: MobileCenterClient): Promise<CommandResult> {
+    const app = this.app;
+
+    let releases: models.BasicReleaseDetails[];
+    try {
+      const httpRequest = await out.progress("Loading list of releases...", clientRequest<models.BasicReleaseDetails[]>(
+        (cb) => client.distribute.getReleases(app.ownerName, app.appName, {
+          publishedOnly: true
+        }, cb)));
+      releases = httpRequest.result;
+    } catch (error) {
+      debug(`Failed to get list of releases for the application - ${inspect(error)}`);
+      return failure(ErrorCodes.Exception, "failed to get list of releases for the application");
+    }
+
+    out.reportNewLineSeparatedArray([
+        ["ID", "id"],
+        ["Short Version", "shortVersion"],
+        ["Version", "version"],
+        ["Uploaded At", "uploadedAt", out.report.asDate]
+      ], releases);
+   
+    return success();
+  }
+}
