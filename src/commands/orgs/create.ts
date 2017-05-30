054a8cfebc198af253cef9189a6dfbc736529580
Dmitry Bryukov <v-dmbry@microsoft.com>
2017-05-30 20:25:07 +0300

    Implemented "orgs create" command

---
+++ src/commands/orgs/create.ts
@@ -0,0 +1,69 @@
+import { Command, CommandResult, help, success, failure, ErrorCodes, shortName, longName, hasArg, required } from "../../util/commandline";
+import { out } from "../../util/interaction";
+import { MobileCenterClient, models, clientRequest } from "../../util/apis";
+
+const debug = require("debug")("mobile-center-cli:commands:orgs:create");
+import { inspect } from "util";
+import { getPortalOrgLink } from "../../util/portal/portal-helper";
+
+@help("Create a new organization")
+export default class OrgCreateCommand extends Command {
+  @help("Name of the organization")
+  @shortName("n")
+  @longName("name")
+  @required
+  @hasArg
+  name: string;
+
+  @help("Display name of the organization")
+  @shortName("d")
+  @longName("display-name")
+  @hasArg
+  displayName: string;
+
+  async run(client: MobileCenterClient, portalBaseUrl: string): Promise<CommandResult> {
+    
+    let organizationInfo: models.OrganizationResponse;
+    try {
+      const httpResponse = await out.progress("Creating new organization...", clientRequest<models.OrganizationResponse>((cb) => client.organization.createOrUpdate({
+        displayName: this.displayName,
+        name: this.name
+      }, cb)));
+      if (httpResponse.response.statusCode < 400) {
+        organizationInfo = httpResponse.result;        
+      } else {
+        throw httpResponse.response;
+      }
+    } catch (error) {
+      if (error.statusCode === 409) {
+        return failure(ErrorCodes.InvalidParameter, `organization ${this.name} already exists`);
+      } else {
+        debug(`Failed to create organization - ${inspect(error)}`);
+        return failure(ErrorCodes.Exception, "failed to create organization");
+      }
+    }
+
+    let admins: models.OrganizationUserResponse[];
+    try {
+      const httpResponse = await out.progress("Loading list of users...", clientRequest<models.OrganizationUserResponse[]>((cb) => client.users.listForOrg(organizationInfo.name, cb)));
+      if (httpResponse.response.statusCode < 400) {
+        admins = httpResponse.result.filter((user) => user.role === "admin");        
+      } else {
+        throw httpResponse.response;
+      }
+    } catch (error) {
+      debug(`Failed to get list of organization users - ${inspect(error)}`);
+      return failure(ErrorCodes.Exception, "failed to load list of organization users");
+    }
+
+    out.text(`Successfully created organization ${this.name}`);
+    out.report([
+      ["Name", "name"],
+      ["Display name", "displayName"],
+      ["URL", "url"],
+      ["Admins", "admins", (adminsArray: models.OrganizationUserResponse[]) => adminsArray.map((admin) => admin.name).join(", ")]
+    ], { name: this.name, displayName: this.displayName, url: getPortalOrgLink(portalBaseUrl, this.name), admins});
+
+    return success();
+  }
+}
