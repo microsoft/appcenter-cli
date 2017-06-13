ecc2dc5741944b84e6818b1042a5b6950ae23456
Dmitry Bryukov <v-dmbry@microsoft.com>
2017-06-13 23:05:43 +0300

    Moved portal endpoints strings to environmentsData

--- src/util/portal/portal-helper.ts
+++ src/util/portal/portal-helper.ts
@@ -1,14 +1,3 @@
-let portalMap: Map<string, string> = new Map<string, string>([
-  ["https://bifrost-int.trafficmanager.net", "https://asgard-int.trafficmanager.net"],
-  ["https://bifrost-dev.trafficmanager.net", "https://asgard-dev.trafficmanager.net"],
-  ["https://bifrost-staging.trafficmanager.net", "https://asgard-staging.trafficmanager.net"],
-  ["https://api.mobile.azure.com", "https://mobile.azure.com"]
-]);
-
-export function getPortalBaseUrl(apiBasename: string): string {
-  return portalMap.has(apiBasename) ? portalMap.get(apiBasename) : apiBasename;
-}
-
 export function getPortalBuildLink(portalBaseUrl: string, appOwner: string, appName: string, branchName: string, buildId: string): string {
   return `${portalBaseUrl}/users/${appOwner}/apps/${appName}/build/branches/${branchName}/builds/${buildId}`;
 }
