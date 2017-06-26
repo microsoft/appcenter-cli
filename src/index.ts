bff2f498449b77731a68bb2d37f0f6b496d7c88a
Dmitry Bryukov <v-dmbry@microsoft.com>
2017-06-27 01:25:12 +0300

    Changed error message prefix to red "Error:" string

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,13 @@ import {
 } from "./util/commandline";
 
 import * as path from "path";
+import * as chalk from "chalk";
 
 let runner = commandRunner(path.join(__dirname, "commands"));
 runner(process.argv.slice(2))
   .then((result: CommandResult) => {
     if (failed(result)) {
-      console.log(`Command failed: ${result.errorMessage}`);
+      console.log(`${chalk.bold.red("Error:")} ${result.errorMessage}`);
       process.exit(result.errorCode);
     }
   });
