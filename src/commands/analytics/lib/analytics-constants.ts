dbc640edda5522cd414fa32dc92cddbfa94aab28
David Siegel <djsiegel@gmail.com>
2017-07-06 15:24:58 -0700

    Remove nested parentheses and simplify

--- src/commands/analytics/lib/analytics-constants.ts
+++ src/commands/analytics/lib/analytics-constants.ts
@@ -1,3 +1,3 @@
-export const startDateHelpMessage = "Start date (e.g. '1970/01/01 00:00' (system time zone), RFC2822 and ISO 8601 date strings are also supported). Default value: the beginning of current UTC day";
+export const startDateHelpMessage = "Start date. '1970/01/01 00:00' (system time zone), RFC2822, and ISO 8601 dates are supported. Defaults to start of current UTC day";
 
-export const endDateHelpMessage = "End date (e.g. '1970/01/01 00:00' (system time zone), RFC2822 and ISO 8601 date strings are also supported). Default value: now";
+export const endDateHelpMessage = "End date. '1970/01/01 00:00' (system time zone), RFC2822, and ISO 8601 dates are supported. Defaults to now";
