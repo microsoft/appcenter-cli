a34cfa6cf3af8a84897efb55bce473aa56a693f5
BO KANG <bokan@microsoft.com>
2017-08-04 11:17:28 -0700

    add error message to help users use --app parameter to specify app, clean the code.

--- src/commands/codepush/deployment/list.ts
+++ src/commands/codepush/deployment/list.ts
@@ -2,8 +2,9 @@ import { AppCommand, CommandArgs, CommandResult, help, failure, ErrorCodes, succ
 import { out } from "../../../util/interaction";
 import { DefaultApp } from "../../../util/profile";
 import { inspect } from "util";
-import { MobileCenterClient, models, clientRequest } from "../../../util/apis";
+import { MobileCenterClient, models, clientRequest, clientCall } from "../../../util/apis";
 const _ = require("lodash");
+const chalk = require("chalk");
 
 const debug = require("debug")("mobile-center-cli:commands:codepush:deployments:list");
 
@@ -17,14 +18,19 @@ export default class ListCommand extends AppCommand {
     const app = this.app;
     let deployments: models.Deployment[];
     try {
-      const httpRequest = await out.progress("Getting codepush deployments ...", clientRequest<models.Deployment[]>(
+      const httpRequest = await out.progress("Getting codepush deployments...", clientRequest<models.Deployment[]>(
         (cb) => client.deployments.list(app.ownerName, app.appName, cb)));
       deployments = httpRequest.result;
+      out.table(out.getCommandOutputTableOptions(["Name", "Key"]), deployments.map((deployment) => [deployment.name, deployment.key]));
+      return success();
     } catch (error) {
       debug(`Failed to get list of codepush deployments - ${inspect(error)}`);
-      return failure(ErrorCodes.Exception, "failed to get list of deployments for the app");
+      if (error.statusCode === 404) {
+        const appNotFoundErrorMsg = "The app name does not exist. Please double check the name, and provide it in the form owner/appname. \nRun the command " + chalk.bold("mobile-center apps list") + " to see what apps you have access to.";
+        return failure(ErrorCodes.InvalidParameter, appNotFoundErrorMsg);
+      } else {        
+        return failure(ErrorCodes.Exception, "failed to get list of deployments for the app");
+      }
     }
-    out.table(out.getCommandOutputTableOptions(["Name", "Key"]), deployments.map((deployment) => [deployment.name, deployment.key]));
-    return success();
   }
 }
