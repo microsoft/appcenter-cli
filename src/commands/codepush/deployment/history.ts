6a8a2f2a9c00f20f3f90f8ce6e51e32733862d98
BO KANG <bokan@microsoft.com>
2017-08-07 16:29:37 -0700

    fix typos

--- src/commands/codepush/deployment/history.ts
+++ src/commands/codepush/deployment/history.ts
@@ -33,12 +33,12 @@ export default class HistoryCommand extends AppCommand {
       out.table(out.getCommandOutputTableOptions(["Label", "Release Time", "App Version", "Mandatory", "Description"]), releases.map((release) => [release.label, formatDate(release.uploadTime), release.targetBinaryRange, release.isMandatory, release.description]));
       return success();
     } catch (error) {
-      debug(`Failed to get list of Codepush deployments - ${inspect(error)}`);
+      debug(`Failed to get list of CodePush deployments - ${inspect(error)}`);
       if (error.statusCode === 404) {
         const appNotFoundErrorMsg = `The app ${app.ownerName}/${app.appName} does not exist. Please double check the name, and provide it in the form owner/appname. \nRun the command ${chalk.bold("mobile-center apps list")} to see what apps you have access to.`;
         return failure(ErrorCodes.InvalidParameter, appNotFoundErrorMsg);
       } else if (error.statusCode === 400) {
-        const deploymentNotExistErrorMsg = `The deployment ${chalk.bold(this.deploymentName)} did not exist.`;
+        const deploymentNotExistErrorMsg = `The deployment ${chalk.bold(this.deploymentName)} does not exist.`;
         return failure(ErrorCodes.Exception, deploymentNotExistErrorMsg);
       } else {
         return failure(ErrorCodes.Exception, "failed to get list of releases for the app");
