b76a0af672a851edf70f8ad9e1b7891cf6be7b1f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2017-11-30 20:30:27 +0900

    Allow versions like '1' or '1.2' too
    ref: https://github.com/Microsoft/appcenter-cli/pull/331#discussion_r154049624

--- src/commands/codepush/lib/validation-utils.ts
+++ src/commands/codepush/lib/validation-utils.ts
@@ -4,12 +4,12 @@ import { AppCenterClient, models, clientRequest } from "../../../util/apis";
 import { DefaultApp } from "../../../util/profile";
 
 // Check if the given string is a semver-compliant version number (e.g. '1.2.3')
+// (missing minor/patch values will be added on server side to pass semver.satisfies check)
 export function isValidVersion(version: string): boolean {
-  return !!semver.valid(version);
+  return !!semver.valid(version) || /^\d+\.\d+$/.test(version) || /^\d+$/.test(version);
 }
 
 // Allow plain integer versions (as well as '1.0' values) for now, e.g. '1' is valid here and we assume that it is equal to '1.0.0'. 
-// (missing minor/patch values will be added on server side to pass semver.satisfies check)
 export function isValidRange(semverRange: string): boolean {
   return !!semver.validRange(semverRange);
 }
