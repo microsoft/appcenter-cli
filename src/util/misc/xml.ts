e4b04037bf0c9209ac9bdeba43fb1e6b6f20572b
Evgeny Rychagov <v-evryc@microsoft.com>
2018-03-05 17:18:31 +0300

    Change xml package Rewrite xml processing code using new package Merge results to single xml files

--- src/util/misc/xml.ts
+++ src/util/misc/xml.ts
@@ -1,57 +1,50 @@
-import * as xmlParser from "xml-parser";
+import {  } from "xmldom";
+import { child } from "event-stream";
 
-interface Node {
-  child: xmlParser.Node;
-  parent: xmlParser.Node;
-}
-
-export function appendToTestNameTransformation(xml: xmlParser.Document, text: string) {
-  let testCases: xmlParser.Node[] = [];
-  collectAllElements(xml.root, "test-case", testCases);
-  testCases.forEach((testCase: xmlParser.Node) => {
-    if(testCase.attributes["name"]) {
-      testCase.attributes["name"] = `${testCase.attributes["name"]}${text}`;
+export function appendToTestNameTransformation(xml: Document, text: string) {
+  let testCases: Node[] = collectAllElements(xml, "test-case");
+  testCases.forEach((testCase: Node) => {
+    let name: Attr = testCase.attributes.getNamedItem("name");
+    if(name) {
+      name.value = `${name.value}${text}`; // TODO: value or nodeValue?
     }
   });
 }
 
-export function removeIgnoredTransformation(xml: xmlParser.Document) {
-  var testResults: xmlParser.Node[] = [];
-  collectAllElements(xml.root, "test-results", testResults);
-  testResults.forEach((testResult: xmlParser.Node) => {
-    let ignored: number = Number(testResult.attributes["ignored"]);
-    if(ignored) {
-      testResult.attributes["ignored"] = "0";
-
-      let notRun: number = Number(testResult.attributes["not-run"]);
-      if(notRun) {
-        testResult.attributes["not-run"] = String(notRun - ignored);
+export function removeIgnoredTransformation(xml: Document) {
+  var testResults: Node[] = collectAllElements(xml, "test-results");
+  testResults.forEach((testResult: Node) => {
+    let ignoredAttr: Attr = testResult.attributes.getNamedItem("ignored");
+    if (ignoredAttr) {
+      let notRunAttr: Attr = testResult.attributes.getNamedItem("not-run");
+      if (notRunAttr) {
+        let notRun = Number(notRunAttr.value);
+        let ignored = Number(ignoredAttr.value);
+        notRunAttr.value = String(notRun - ignored);
       }
+      ignoredAttr.value = "0";
     }
   });
 
-  var nodes: Node[] = [];
-  collectAllElementsWithParent(xml.root, "test-case", nodes);
+  var nodes: Node[] = collectAllElements(xml, "test-case");
   nodes.forEach((node: Node) => {
-    if(node.child.attributes["result"] === "Ignored") {
-      let index = node.parent.children.indexOf(node.child);
-      node.parent.children.slice(index,1);
+    let resultAttr = node.attributes.getNamedItem("result");
+    if(resultAttr && resultAttr.value === "Ignored") {
+      node.parentNode.removeChild(node);
     }
   });
 }
 
-export function removeEmptySuitesTransformation(xml: xmlParser.Document) {
-  var nodes: Node[] = [];
-  collectAllElementsWithParent(xml.root, "test-suite", nodes);
+export function removeEmptySuitesTransformation(xml: Document) {
+  var nodes: Node[] = collectAllElements(xml, "test-suite");
   nodes.forEach((node: Node) => {
-    if (countChildren(node.child) <= 1) {
-      let index = node.parent.children.indexOf(node.child);
-      node.parent.children.slice(index,1);
+    if (countChildren(node) <= 1) {
+      node.parentNode.removeChild(node);
     }
   });
 }
 
-export function combine(xml1: xmlParser.Document, xml2: xmlParser.Document): xmlParser.Document {
+export function combine(xml1: Document, xml2: Document): Document {
   combineTestResultsAttribute(xml1, xml2, "total");
   combineTestResultsAttribute(xml1, xml2, "errors");
   combineTestResultsAttribute(xml1, xml2, "failures");
@@ -61,59 +54,68 @@ export function combine(xml1: xmlParser.Document, xml2: xmlParser.Document): xml
   combineTestResultsAttribute(xml1, xml2, "skipped");
   combineTestResultsAttribute(xml1, xml2, "invalid");
 
-  xml2.root.children.forEach((child: xmlParser.Node) => {
-    if(child.name === "test-suite") {
-      xml1.root.children.push(child);
-    }
+  let testSuitesParent: Node = collectAllElements(xml1, "test-results")[0];
+  let testSuites: Node[] = collectChildren(xml2, "test-suite");
+
+  testSuites.forEach((child: Node) => {
+    testSuitesParent.appendChild(child);
   });
 
   return xml1;
 }
 
-function collectAllElements(node: xmlParser.Node, name: string, result: xmlParser.Node[]) {
-  if(node.name === name) {
+function collectAllElements(node: Node, name: string): Node[] {
+  let result: Node[] = [];
+  if(node.nodeName === name) {
     result.push(node);
   }
-  node.children.forEach((element: xmlParser.Node) => {
-    collectAllElements(element, name, result);
-  });
-}
-
-function collectAllElementsWithParent(node: xmlParser.Node, name: string, result: Node[]) {
-  node.children.forEach((element: xmlParser.Node) => {
-    if(element.name === name) {
-      result.push({ child: element, parent: node });
-    }
-    collectAllElementsWithParent(element, name, result);
-  })
-}
-
-function countChildren(node: xmlParser.Node): number {
-  let result = node.children.length;
-  node.children.forEach((child: xmlParser.Node) => {
-    result += countChildren(child);
-  })
+  for(let i: number = 0; node.childNodes && i < node.childNodes.length; ++i) {
+    result = result.concat(collectAllElements(node.childNodes[i], name));
+  }
   return result;
 }
 
-function combineTestResultsAttribute(xml1: xmlParser.Document, xml2: xmlParser.Document, attributeName: string) {
+function collectChildren(node: Node, name: string): Node[] {
+  if (node.nodeName === name) {
+    return [node];
+  }
+  let result: Node[] = [];
+  for(let i: number = 0; node.childNodes && i < node.childNodes.length; ++i) {
+    result = result.concat(collectChildren(node.childNodes[i], name));
+  }
+  return result;
+}
+
+function countChildren(node: Node): number {
+  if (!node.childNodes) {
+    return 0;
+  }
+  let result = node.childNodes.length;;
+  for(let i: number = 0; i < node.childNodes.length; ++i) {
+    result += countChildren(node.childNodes[i]);
+  }
+  return result;
+}
+
+function combineTestResultsAttribute(xml1: Document, xml2: Document, attributeName: string) {
   addTestResultsAttribute(xml1, attributeName, getTestResultsAttribute(xml2, attributeName));
 }
 
-function getTestResultsAttribute(xml: xmlParser.Document, attributeName: string): number {
-  let testResults: xmlParser.Node[] = [];
-  collectAllElements(xml.root, "test-results", testResults);
+function getTestResultsAttribute(xml: Document, attributeName: string): number {
+  let testResults: Node[] = collectAllElements(xml, "test-results");
 
-  let value: number = Number(testResults[0].attributes[attributeName]);
-  if(value) {
-    return value;
+  let attr: Attr = testResults[0].attributes.getNamedItem(attributeName);
+  if(attr.value) {
+    return Number(attr.value);
   }
   return 0;
 }
 
-function addTestResultsAttribute(xml: xmlParser.Document, attributeName: string, value: number) {
+function addTestResultsAttribute(xml: Document, attributeName: string, value: number) {
   let currentValue = getTestResultsAttribute(xml, attributeName);
-  let testResults: xmlParser.Node[] = [];
-  collectAllElements(xml.root, "test-results", testResults);
-  testResults[0].attributes[attributeName] = String(currentValue + value);
+  let testResults: Node[] = collectAllElements(xml, "test-results");
+  let attr: Attr = testResults[0].attributes.getNamedItem(attributeName);
+  if (attr) {
+    attr.value = String(currentValue + value);
+  }
 }
