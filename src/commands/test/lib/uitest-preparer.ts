6aea5a353d90fa164421e6f0f72d6ea4e0546eb6
Owen Niblock <me@owenniblock.com>
2018-05-01 12:04:35 +0100

    Fixes typo on help text

--- src/commands/test/lib/uitest-preparer.ts
+++ src/commands/test/lib/uitest-preparer.ts
@@ -194,7 +194,7 @@ export class UITestPreparer {
     const testCloudPathDev = path.join(toolsDir, "Xamarin.UITest.Console.exe");
 
     if (!await fileExists(testCloudPath) && !await fileExists(testCloudPathDev)) {
-      throw new Error(`Cannot find test-cloud.exe.., the exe was not found in the path specified by "--uitest-tools-dir".${os.EOL}` +
+      throw new Error(`Cannot find test-cloud.exe, the exe was not found in the path specified by "--uitest-tools-dir".${os.EOL}` +
         `Please check that ${testCloudPath} points to a test-cloud.exe.${os.EOL}` +
         `Minimum required version is "${this.getMinimumVersionString()}".`);
     }
