9904b7ebe4c4746d8a142b3fe62e4d46b2fa12b1
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-05-17 14:50:28 +0300

    Xamarin wizard

--- src/commands/test/lib/wizard/uitests.ts
+++ src/commands/test/lib/wizard/uitests.ts
@@ -1,7 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
 import { CommandArgs, CommandResult, AppCommand } from "../../../../util/commandline";
 import { AppCenterClient } from "../../../../util/apis";
-import { out } from "../../../../util/interaction";
+import { prompt, out } from "../../../../util/interaction";
 import RunUITestsCommand from "../../run/uitest";
+import { Questions } from "inquirer";
+
+interface BuildFolder {
+  name: string;
+  path: string;
+}
 
 export default class RunUitestWizardTestCommand extends AppCommand {
 
@@ -13,7 +21,109 @@ export default class RunUitestWizardTestCommand extends AppCommand {
   }
 
   public async run(client: AppCenterClient, portalBaseUrl: string): Promise<CommandResult> {
+    const searchFolder: Promise<BuildFolder[]> = this.scanFolder();
+    if (this._args.args.indexOf("--async") < 0) {
+      const mergeXml: boolean = await this.promptMergeXml();
+      if (mergeXml) {
+        this._args.args.push("--merge-nunit-xml");
+      }
+    }
+    const foundFolders: BuildFolder[] = await searchFolder;
+    const folder: string = await this.promptFolder(foundFolders);
+    this._args.args.push("--build-dir", folder);
     out.text("\nRunning command: appcenter test run uitest " + this._args.args.join(" ") + "\n");
     return new RunUITestsCommand(this._args).run(client, portalBaseUrl);
+  }
+
+  private async promptMergeXml(): Promise<boolean> {
+    const questions: Questions = [
+      {
+        type: "list",
+        name: "merge",
+        message: "Should the xml files be merged in to the <output.xml> file?",
+        choices: [{
+          name: "Yes",
+          value: "true"
+        }, {
+          name: "No",
+          value: "false"
+        }]
+      }
+    ];
+    const answers: any = await prompt.question(questions);
+    return answers.merge === "true" ? true : false;
+  }
+
+  private async scanFolder(): Promise<BuildFolder[]> {
+    const foundFolders: BuildFolder[] = [];
+    this.scanRecurse(process.cwd(), foundFolders);
+    return foundFolders;
+  }
+
+  private scanRecurse(dirname: string, folders: BuildFolder[]) {
+    const dirContent = fs.readdirSync(dirname);
+    for (const dir of dirContent) {
+      const fullDir = path.join(dirname, dir);
+      if (fs.lstatSync(fullDir).isDirectory()) {
+        if (dir !== "node_modules") {
+          if (fs.readdirSync(fullDir).length > 0) {
+            const dirContents: string[] = fs.readdirSync(fullDir);
+
+            const configs = dirContents.filter((file) => {
+              return file === "packages.config";
+            });
+            const projects = dirContents.filter((file) => {
+              return path.parse(file).ext === ".csproj" || path.parse(dir).ext === ".fsproj";
+            });
+            const containsConfigAndProjectFiles: boolean = configs.length > 0 && projects.length > 0;
+
+            if (containsConfigAndProjectFiles) {
+              const foundFolder: BuildFolder = {
+                name: path.relative(process.cwd(), fullDir.split(dir)[0]),
+                path: fullDir.split(dir)[0]
+              };
+              if (!folders) {
+                folders = [foundFolder];
+              } else {
+                folders.push(foundFolder);
+              }
+            } else {
+              this.scanRecurse(fullDir, folders);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private async promptFolder(listOfFolders: BuildFolder[]): Promise<string> {
+
+    if (listOfFolders.length) {
+      const choices = listOfFolders.map((folder) => {
+        return {
+          name: folder.name,
+          value: folder.path
+        };
+      });
+      choices.push({
+        name: "Enter path manually",
+        value: "manual"
+      });
+      const questions: Questions = [
+        {
+          type: "list",
+          name: "folderPath",
+          message: "Pick a folder with the Xamarin tests",
+          choices: choices
+        }
+      ];
+      const answers: any = await prompt.question(questions);
+      if (answers.apkPath === "manual") {
+        return await prompt("Please provide the path to the Xamarin tests.");
+      }
+      return answers.folderPath;
+    } else {
+      return await prompt("We could not find any folders with Xamarin tests. Please provide the path to them.");
+    }
   }
 }
