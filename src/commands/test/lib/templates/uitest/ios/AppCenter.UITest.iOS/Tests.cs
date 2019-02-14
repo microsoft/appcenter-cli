1329f02da0397046a69a172efa8333d900802f4c
Jonathan Stoneman <jon@stoneman.me.uk>
2019-02-14 13:43:13 +0000

    Update launch tests to NUnit 3

--- src/commands/test/lib/templates/uitest/ios/AppCenter.UITest.iOS/Tests.cs
+++ src/commands/test/lib/templates/uitest/ios/AppCenter.UITest.iOS/Tests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.iOS;
 
@@ -20,39 +17,7 @@ namespace AppCenter.UITest.iOS
         [Test]
         public void AppDoesLaunch()
         {
-        }
-
-        // [Test]
-        [Ignore]
-        public void TapRandomButtonsTest()
-        {
-            var rand = new Random();
-
-            for (var i = 0; i < 10; i++)
-            {
-                var buttons = _app.Query(c => c.Button()).ToArray();
-
-                if (!buttons.Any())
-                {
-                    break;
-                }
-
-                try
-                {
-                    _app.Tap(c => c.Button().Index(rand.Next(0, buttons.Length - 1)));
-                }
-                catch
-                {
-                    // Fail silently, this is probably due to the number of buttons on the page being reduced between
-                    // the app.Query() and the app.Tap().
-                }
-
-                var word = i == 0 ? "a" : "another";
-
-                _app.Screenshot($"Tapped {word} random button");
-
-                Thread.Sleep(3000);  // This should allow any animations to complete in most cases
-            }
+            _app.WaitForElement(c => c.All());
         }
     }
 }
