d375c391f5e938b56f34861912e706e66e0fa058
Sven Pfleiderer <svpfleid@microsoft.com>
2019-03-15 09:25:42 -0700

    Implement adding a group + most of the tests

--- src/commands/distribute/releases/add-destination.ts
+++ src/commands/distribute/releases/add-destination.ts
@@ -9,6 +9,12 @@ const debug = require("debug")("appcenter-cli:commands:distribute:releases:add-d
 
 const ValidDestinationTypes = ["group", "tester"];
 
+class AddDestinationError extends Error {
+  constructor(message: string, public errorCode: ErrorCodes) {
+    super(message);
+  }
+}
+
 @help("Adds a new destination to an existing release")
 export default class AddDestinationCommand extends AppCommand {
 
@@ -44,40 +50,63 @@ export default class AddDestinationCommand extends AppCommand {
     if (ValidDestinationTypes.indexOf(this.destinationType) === -1) {
       return failure(ErrorCodes.InvalidParameter, `${this.destinationType} is not a valid destination type. Available types are: ${ValidDestinationTypes.join(", ")}`);
     }
-    debug("Distributing the release");
-    if (this.destinationType === "group") {
-      await this.reDistributeGroup(client, app, releaseId, );
+    debug(`Distributing destination ${this.destination} of type ${this.destinationType} to release ${releaseId}`);
 
-    } else if (this.destinationType === "tester") {
-      return;
+    try {
+      await out.progress(`Adding the destination to the release ...`, this.addDestination(client, releaseId));
+    } catch (error) {
+      if (error.errorCode !== ErrorCodes.InvalidParameter) {
+        debug(`Failed to add destination - ${inspect(error)}`);
+      }
+      return failure(error.errorCode, error.message);
     }
-
-    // TODO: Actually call client to add destination ; this is blocked on https://msmobilecenter.visualstudio.com/Mobile-Center/_git/appcenter/pullrequest/24972
 
     return success();
   }
 
-  private async reDistributeGroup(client: AppCenterClient, app: DefaultApp, releaseId: number, mandatoryUpdate: boolean = false, notifyTesters: boolean= true): Promise<models.ReleaseDetailsResponse> {
-    let updateReleaseRequestResponse: ClientResponse<models.ReleaseDetailsResponse>;
+  private async addDestination(client: AppCenterClient, releaseId: number): Promise<void> {
+    if (this.destinationType === "group") {
+      const distributionGroup = await this.getDistributionGroup(client, releaseId);
+      await this.addGroupToRelease(client, distributionGroup, releaseId);
+    } else if (this.destinationType === "tester") {
+      return;
+    }
+  }
+
+  private async getDistributionGroup(client: AppCenterClient, releaseId: number): Promise<models.DistributionGroupResponse> {
     try {
-      updateReleaseRequestResponse = await out.progress(`Distributing the release...`,
-        clientRequest<models.ReleaseDetailsResponse>(async (cb) => client.releases.addDistributionGroup(releaseId, app.ownerName, app.appName, "groupId", {
-          mandatoryUpdate: false,
-          notifyTesters: false,
-        }, cb)));
-      const statusCode = updateReleaseRequestResponse.response.statusCode;
-      if (statusCode >= 400) {
-        throw statusCode;
-      }
+      const { result } = await clientRequest<models.DistributionGroupResponse>(async (cb) => {
+        client.distributionGroups.get(this.app.ownerName, this.app.appName, this.destination, cb);
+      });
+
+      return result;
     } catch (error) {
-      if (error === 400) {
-        throw failure(ErrorCodes.Exception, "errorr message");
+      if (error.statusCode === 404) {
+        throw new AddDestinationError(`Could not find group ${this.destination}`, ErrorCodes.InvalidParameter);
       } else {
         debug(`Failed to distribute the release - ${inspect(error)}`);
-        throw failure(ErrorCodes.Exception, `failed to set distribution group and release notes for release ${releaseId}`);
+        throw new AddDestinationError(`Could not add ${this.destinationType} ${this.destination} to release ${releaseId}`, ErrorCodes.Exception);
       }
     }
+  }
 
-    return updateReleaseRequestResponse.result;
+  private async addGroupToRelease(client: AppCenterClient, distributionGroup: models.DistributionGroupResponse, releaseId: number): Promise<models.ReleaseDestinationResponse> {
+    try {
+      const { result } = await clientRequest<models.ReleaseDestinationResponse>(async (cb) => {
+        client.releases.addDistributionGroup(releaseId, this.app.ownerName, this.app.appName, distributionGroup.id, {
+          mandatoryUpdate: false,
+          notifyTesters: false,
+        }, cb);
+      });
+
+      return result;
+    } catch (error) {
+      if (error.statusCode === 404) {
+        throw new AddDestinationError(`Could not find release ${releaseId}`, ErrorCodes.InvalidParameter);
+      } else {
+        debug(`Failed to distribute the release - ${inspect(error)}`);
+        throw new AddDestinationError(`Could not add ${this.destinationType} ${this.destination} to release ${releaseId}`, ErrorCodes.Exception);
+      }
+    }
   }
 }
--- test/commands/distribute/releases/add-destination-test.ts
+++ test/commands/distribute/releases/add-destination-test.ts
@@ -14,9 +14,19 @@ describe("releases add-destination command", () => {
   const fakeToken = "c1o3d3e7";
   const fakeReleaseId = "1";
   const fakeDistributionGroupName = "fakeDistributionGroupName";
+  /* tslint:disable-next-line:no-http-string */
+  const fakeHost = "http://localhost:1700";
+  const getDistributionGroupUrl = `/v0.1/apps/${fakeAppOwner}/${fakeAppName}/distribution_groups/${fakeDistributionGroupName}`;
+  const postAddReleaseGroupDestinationUrl = `/v0.1/apps/${fakeAppOwner}/${fakeAppName}/releases/${fakeReleaseId}/groups`;
+
+  let nockScope: Nock.Scope;
 
   before(() => {
     Nock.disableNetConnect();
+  });
+
+  beforeEach(() => {
+    nockScope = Nock(fakeHost);
   });
 
   afterEach(() => {
@@ -27,16 +37,45 @@ describe("releases add-destination command", () => {
     Nock.enableNetConnect();
   });
 
-  describe("when all needed information is provided", () => {
-    return;
-  });
-
   describe("when everything works as expected", function () {
+    beforeEach(() => {
+      nockScope.get(getDistributionGroupUrl)
+        .reply(200, {
+          id: "00000000-0000-0000-0000-000000000000",
+          name: fakeDistributionGroupName,
+          dismay_name: "my group",
+          origin: "appcenter",
+          is_public: false
+        });
+
+        nockScope.post(postAddReleaseGroupDestinationUrl)
+        .reply(201, {
+        });
+    });
+
     it("reports the command as succeeded", async () => {
       const command = new AddDestinationCommand(getCommandArgs(["--release-id", fakeReleaseId, "--type", "group", "--destination", fakeDistributionGroupName]));
       const result = await command.execute();
 
       expect(result.succeeded).to.be.true;
+
+      nockScope.done();
+    });
+  });
+
+  describe("when the distribution group does not exist", () => {
+    beforeEach(() => {
+      nockScope.get(getDistributionGroupUrl)
+        .reply(404, {});
+    });
+
+    it("reports the command as failed", async () => {
+      const command = new AddDestinationCommand(getCommandArgs(["--release-id", fakeReleaseId, "--type", "group", "--destination", fakeDistributionGroupName]));
+      const result: CommandFailedResult = await command.execute() as CommandFailedResult;
+
+      expect(result.succeeded).to.be.false;
+      expect(result.errorCode).to.eql(ErrorCodes.InvalidParameter);
+      expect(result.errorMessage).to.eql(`Could not find group ${fakeDistributionGroupName}`);
     });
   });
 
