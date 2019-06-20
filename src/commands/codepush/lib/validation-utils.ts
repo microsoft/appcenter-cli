6641babd3b5dae1d24fda7e0a6a833df8c70e096
alexandergoncharov <alexander.goncharov@akvelon.com>
2019-06-20 13:16:03 +0300

    Added warning for poor target version

--- src/commands/codepush/lib/validation-utils.ts
+++ src/commands/codepush/lib/validation-utils.ts
@@ -2,10 +2,13 @@ import * as semver from "semver";
 import { AppCenterClient, models, clientRequest } from "../../../util/apis";
 import { DefaultApp } from "../../../util/profile";
 
+const regexpForMajor = /^\d+$/;
+const regexpForMajorMinor = /^\d+\.\d+$/;
+
 // Check if the given string is a semver-compliant version number (e.g. '1.2.3')
 // (missing minor/patch values will be added on server side to pass semver.satisfies check)
 export function isValidVersion(version: string): boolean {
-  return !!semver.valid(version) || /^\d+\.\d+$/.test(version) || /^\d+$/.test(version);
+  return !!semver.valid(version) || regexpForMajorMinor.test(version) || regexpForMajor.test(version);
 }
 
 // Allow plain integer versions (as well as '1.0' values) for now, e.g. '1' is valid here and we assume that it is equal to '1.0.0'.
@@ -22,4 +25,14 @@ export async function isValidDeployment(client: AppCenterClient, app: DefaultApp
     (cb) => client.codePushDeployments.get(deploymentName, app.ownerName, app.appName, cb));
 
   return httpRequest.response.statusCode === 200 ? Promise.resolve(true) : Promise.resolve(false);
+}
+
+export function generateWarningVersionForPoorVersionIfNeeded(version: string): string {
+  if (regexpForMajorMinor.test(version)) {
+    return version + ".X"
+  } else if (regexpForMajor.test(version)) {
+    return version + ".X.X";
+  } else {
+    return null;
+  }
 }
