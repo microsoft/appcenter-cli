c6bac87d84ae1477f284f9b9ae74da97febf6d4a
Jeroen Rietveld <jerietve@microsoft.com>
2019-09-30 18:11:59 +0900

    Add store support to `distribute releases add-destination`. (#656)
    Distribute an existing release to a store using `distribute releases add-destination --type store`.

--- src/commands/distribute/releases/add-destination.ts
+++ src/commands/distribute/releases/add-destination.ts
@@ -3,42 +3,43 @@ import { AppCenterClient, models, clientRequest } from "../../../util/apis";
 import { out } from "../../../util/interaction";
 import { inspect } from "util";
 import * as _ from "lodash";
-import { getDistributionGroup, addGroupToRelease } from "../lib/distribute-util";
+import { getDistributionGroup, getExternalStoreToDistributeRelease, addGroupToRelease } from "../lib/distribute-util";
+import { IncomingMessage } from "http";
 
 const debug = require("debug")("appcenter-cli:commands:distribute:releases:add-destination");
 
-const ValidDestinationTypes = ["group", "tester"];
+const ValidDestinationTypes = ["store", "group", "tester"];
 
-@help("Distributes an existing release to an additional destination")
+@help("Distribute an existing release to an additional destination")
 export default class AddDestinationCommand extends AppCommand {
 
-  @help("Release ID")
+  @help("The ID of the release")
   @shortName("r")
   @longName("release-id")
   @required
   @hasArg
   public releaseId: string;
 
-  @help("Destination Type: " + ValidDestinationTypes.join(", "))
+  @help("The type of destination: [" + ValidDestinationTypes.join(", ") + "]")
   @shortName("t")
   @longName("type")
   @required
   @hasArg
   public destinationType: string;
 
-  @help("Destination: The name of the group or the email of a tester")
+  @help("The name of the store or group, or the email of the tester")
   @shortName("d")
   @longName("destination")
   @required
   @hasArg
   public destination: string;
 
-  @help("Mandatory")
+  @help("Whether the release is mandatory for the testers (Not used for stores)")
   @shortName("m")
   @longName("mandatory")
   public mandatory: boolean;
 
-  @help("Silent")
+  @help("If set, do not send a notification to the testers (Not used for stores)")
   @shortName("s")
   @longName("silent")
   public silent: boolean;
@@ -52,19 +53,21 @@ export default class AddDestinationCommand extends AppCommand {
     if (ValidDestinationTypes.indexOf(this.destinationType) === -1) {
       return failure(ErrorCodes.InvalidParameter, `${this.destinationType} is not a valid destination type. Available types are: ${ValidDestinationTypes.join(", ")}`);
     }
-    debug(`Distributing destination ${this.destination} of type ${this.destinationType} to release ${releaseId}`);
+    debug(`Distributing release ${releaseId} to destination ${this.destination} of type ${this.destinationType} to release ${releaseId}`);
 
     try {
       await this.addDestination(client, releaseId);
     } catch (error) {
       return error;
     }
-    out.text(`Distribution of ${this.mandatory ? "mandatory " : ""}release ${this.releaseId} to ${this.destinationType} was successful ${this.silent ? "without" : "with"} notification`);
+    out.text(`Distribution of ${this.mandatory ? "mandatory " : ""}release ${releaseId} to ${this.destinationType} '${this.destination}' was successful ${this.silent ? "without" : "with"} notification`);
     return success();
   }
 
   private async addDestination(client: AppCenterClient, releaseId: number): Promise<void> {
-    if (this.destinationType === "group") {
+    if (this.destinationType === "store") {
+      await out.progress(`Distributing release to store ${this.destination}...`, this.addStoreToRelease(client, releaseId));
+    } else if (this.destinationType === "group") {
       const distributionGroup = await out.progress(`Fetching distribution group information ...`, getDistributionGroup({
         client, releaseId, app: this.app, destination: this.destination, destinationType: this.destinationType
       }));
@@ -76,6 +79,17 @@ export default class AddDestinationCommand extends AppCommand {
     }
   }
 
+  private async addStoreToRelease(client: AppCenterClient, releaseId: number): Promise<void> {
+    const store = await out.progress("Fetching store information...", getExternalStoreToDistributeRelease({
+      client, app: this.app, storeName: this.destination, releaseId
+    }));
+    const { result, response } = await clientRequest<any>(async (cb) => {
+      client.releases.addStore(releaseId, this.app.ownerName, this.app.appName, store.id, cb);
+    });
+
+    this.handleAddDestinationResponse(result, response);
+  }
+
   private async addTesterToRelease(client: AppCenterClient, releaseId: number) {
     const { result, response } = await clientRequest<models.ReleaseDetailsResponse>(async (cb) => {
       client.releases.addTesters(releaseId, this.app.ownerName, this.app.appName, this.destination, {
@@ -84,13 +98,20 @@ export default class AddDestinationCommand extends AppCommand {
       }, cb);
     });
 
+    this.handleAddDestinationResponse(result, response);
+  }
+
+  private handleAddDestinationResponse(result: any, response: IncomingMessage) {
     if (response.statusCode >= 200 && response.statusCode < 400) {
       return result;
     } else if (response.statusCode === 404) {
-      throw failure(ErrorCodes.InvalidParameter, `Could not find release ${releaseId}`);
+      throw failure(ErrorCodes.InvalidParameter, `Could not find release ${this.releaseId}`);
+    } else if (response.statusCode === 400 && result && result.message) {
+      throw failure(ErrorCodes.InvalidParameter, result.message);
     } else {
       debug(`Failed to distribute the release - ${inspect(result)}`);
-      throw failure(ErrorCodes.Exception, `Could not add ${this.destinationType} ${this.destination} to release ${releaseId}`);
+      const extraInfo = response.statusMessage ? `: ${response.statusMessage}` : "";
+      throw failure(ErrorCodes.Exception, `Could not add ${this.destinationType} ${this.destination} to release ${this.releaseId}${extraInfo}`);
     }
   }
 }
