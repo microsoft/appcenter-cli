22147a42b77c60098339063d23876f658123439d
Jeroen Rietveld <jerietve@microsoft.com>
2019-09-30 11:49:44 +0900

    Undo unrelated change

--- src/commands/distribute/releases/add-destination.ts
+++ src/commands/distribute/releases/add-destination.ts
@@ -18,7 +18,7 @@ export default class AddDestinationCommand extends AppCommand {
   @longName("release-id")
   @required
   @hasArg
-  public releaseId: number;
+  public releaseId: string;
 
   @help("The type of destination: [" + ValidDestinationTypes.join(", ") + "]")
   @shortName("t")
@@ -45,54 +45,54 @@ export default class AddDestinationCommand extends AppCommand {
   public silent: boolean;
 
   public async run(client: AppCenterClient): Promise<CommandResult> {
-    this.releaseId = Number(this.releaseId);
-    if (!Number.isSafeInteger(this.releaseId) || this.releaseId <= 0) {
+    const releaseId = Number(this.releaseId);
+    if (!Number.isSafeInteger(releaseId) || releaseId <= 0) {
       return failure(ErrorCodes.InvalidParameter, `${this.releaseId} is not a valid release id`);
     }
 
     if (ValidDestinationTypes.indexOf(this.destinationType) === -1) {
       return failure(ErrorCodes.InvalidParameter, `${this.destinationType} is not a valid destination type. Available types are: ${ValidDestinationTypes.join(", ")}`);
     }
-    debug(`Distributing release ${this.releaseId} to destination ${this.destination} of type ${this.destinationType} to release ${this.releaseId}`);
+    debug(`Distributing release ${releaseId} to destination ${this.destination} of type ${this.destinationType} to release ${releaseId}`);
 
     try {
-      await this.addDestination(client);
+      await this.addDestination(client, releaseId);
     } catch (error) {
       return error;
     }
-    out.text(`Distribution of ${this.mandatory ? "mandatory " : ""}release ${this.releaseId} to ${this.destinationType} was successful ${this.silent ? "without" : "with"} notification`);
+    out.text(`Distribution of ${this.mandatory ? "mandatory " : ""}release ${releaseId} to ${this.destinationType} was successful ${this.silent ? "without" : "with"} notification`);
     return success();
   }
 
-  private async addDestination(client: AppCenterClient): Promise<void> {
+  private async addDestination(client: AppCenterClient, releaseId: number): Promise<void> {
     if (this.destinationType === "store") {
-      await out.progress(`Distributing release to store ${this.destination}...`, this.addStoreToRelease(client));
+      await out.progress(`Distributing release to store ${this.destination}...`, this.addStoreToRelease(client, releaseId));
     } else if (this.destinationType === "group") {
       const distributionGroup = await out.progress(`Fetching distribution group information ...`, getDistributionGroup({
-        client, releaseId: this.releaseId, app: this.app, destination: this.destination, destinationType: this.destinationType
+        client, releaseId, app: this.app, destination: this.destination, destinationType: this.destinationType
       }));
       await out.progress(`Distributing release to group ${this.destination}...`, addGroupToRelease({
-        client, releaseId: this.releaseId, distributionGroup, app: this.app, destination: this.destination, destinationType: this.destinationType, mandatory: this.mandatory, silent: this.silent
+        client, releaseId, distributionGroup, app: this.app, destination: this.destination, destinationType: this.destinationType, mandatory: this.mandatory, silent: this.silent
       }));
     } else if (this.destinationType === "tester") {
-      await out.progress(`Distributing release to tester ${this.destination}...`, this.addTesterToRelease(client));
+      await out.progress(`Distributing release to tester ${this.destination}...`, this.addTesterToRelease(client, releaseId));
     }
   }
 
-  private async addStoreToRelease(client: AppCenterClient): Promise<void> {
+  private async addStoreToRelease(client: AppCenterClient, releaseId: number): Promise<void> {
     const store = await out.progress("Fetching store information...", getExternalStoreToDistributeRelease({
-      client, app: this.app, storeName: this.destination, releaseId: this.releaseId
+      client, app: this.app, storeName: this.destination, releaseId
     }));
     const { result, response } = await clientRequest<any>(async (cb) => {
-      client.releases.addStore(this.releaseId, this.app.ownerName, this.app.appName, store.id, cb);
+      client.releases.addStore(releaseId, this.app.ownerName, this.app.appName, store.id, cb);
     });
 
     this.handleAddDestinationResponse(result, response);
   }
 
-  private async addTesterToRelease(client: AppCenterClient) {
+  private async addTesterToRelease(client: AppCenterClient, releaseId: number) {
     const { result, response } = await clientRequest<models.ReleaseDetailsResponse>(async (cb) => {
-      client.releases.addTesters(this.releaseId, this.app.ownerName, this.app.appName, this.destination, {
+      client.releases.addTesters(releaseId, this.app.ownerName, this.app.appName, this.destination, {
         mandatoryUpdate: this.mandatory,
         notifyTesters: !this.silent
       }, cb);
