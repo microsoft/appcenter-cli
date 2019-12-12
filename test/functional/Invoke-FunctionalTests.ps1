61447a3cec2ece38e132c5dab8dcda258bd27deb
Jeroen Rietveld <jerietve@microsoft.com>
2019-12-12 12:41:52 +0900

    Add first functional test that uses an account to perform real actions (#718)
    Update the Azure Pipelines config to download the API token for an account in the integration environment from a key vault. It passes that token to the PowerShell script which invokes Pester. That script logs in the user, and then tests run using that account. A single simple test creates an app, checks it worked and deletes it again.

---
+++ test/functional/Invoke-FunctionalTests.ps1
@@ -0,0 +1,48 @@
+#!/usr/bin/env pwsh
+
+<#
+  .SYNOPSIS
+  Installs and invokes Pester to run all CLI functional tests.
+
+  .DESCRIPTION
+  Runs the App Center CLI using Node on the current code in the repository. Signs in to the App Center
+  integration testing environment using the provided token. Installs Pester and calls Pester on the
+  directory the script is in.
+#>
+param
+(
+  # The API token used to connect to the App Center integration environment to run the integration tests
+  [Parameter(Mandatory = $true)]
+  [ValidatePattern("[0-9a-f]{40}")]
+  [string]
+  $Token
+)
+
+function Get-ScriptDirectory {
+  Split-Path -parent $PSCommandPath
+}
+
+function Get-Root {
+  Resolve-Path "$(Get-ScriptDirectory)/../.."
+}
+
+if (!(Get-Module -ListAvailable -Name Pester)) {
+  Write-Host "Installing Pester"
+  Install-Module -Name Pester -Force -SkipPublisherCheck
+}
+
+Import-Module Pester
+
+$workingDir = Get-ScriptDirectory
+
+$env:PATH = "$(Get-ScriptDirectory)/bin:" + $env:PATH
+$env:CLI_ROOT = Get-Root
+
+Write-Host "Signing in to App Center"
+$loginResult = appcenter login --env int --token $Token
+
+if (!$loginResult.StartsWith("Logged in as")) {
+  throw "Login failed: $loginResult"
+}
+
+Invoke-Pester $workingDir -OutputFile $workingDir/testresult.xml -OutputFormat NUnitXML
---
+++ test/functional/commands/apps/Create.Tests.ps1
@@ -0,0 +1,24 @@
+Describe "apps create" {
+  It "creates a new app when all required parameters are provided with valid values" {
+    # Arrange
+    $previousNumberOfApps = (appcenter apps list --output json | ConvertFrom-Json).Count
+    $appDisplayName = "TestAppsCreate$((Get-Date).ToString("yyyy-MM-dd_HH.mm.ffffff"))"
+    $appOs = "Android"
+    $appPlatform = "Java"
+
+    # Act
+    $app = appcenter apps create --platform $appPlatform --os $appOs --display-name $appDisplayName --output json | ConvertFrom-Json
+
+    # Assert
+    $app.displayName | Should -Be $appDisplayName
+    $app.os | Should -Be $appOs
+    $app.platform | Should -Be $appPlatform
+
+    $newNumberOfApps = (appcenter apps list --output json | ConvertFrom-Json).Count
+    $newNumberOfApps | Should -Be ($previousNumberOfApps + 1)
+
+    # Cleanup
+    $appFullName = $app.owner.name + "/" + $app.name
+    appcenter apps delete --app $appFullName --quiet
+  }
+}
