b3798f029e8face06b94014c35b913f696aa9bda
Jean-Philippe ANDRE <jeandr@microsoft.com>
2020-06-10 11:36:41 +0900

    moar debug

--- test/functional/commands/distribute/Release.Tests.ps1
+++ test/functional/commands/distribute/Release.Tests.ps1
@@ -2,7 +2,10 @@ Describe "distribute release" {
   $appDisplayName = "TestDistributeRelease$((Get-Date).ToString("yyyy-MM-dd_HH.mm.ffffff"))"
   $appOs = "Custom"
   $appPlatform = "Custom"
-  $app = appcenter apps create --platform $appPlatform --os $appOs --display-name $appDisplayName --output json | ConvertFrom-Json
+  Write-Host "RUN: appcenter apps create --platform $appPlatform --os $appOs --display-name $appDisplayName --output json"
+  $appRaw = appcenter apps create --platform $appPlatform --os $appOs --display-name $appDisplayName --output json
+  Write-Host "appRaw: $appRaw"
+  $app = $appRaw | ConvertFrom-Json
   $appFullName = $app.owner.name + "/" + $app.name
   appcenter apps set-current $appFullName
 
@@ -12,12 +15,16 @@ Describe "distribute release" {
     "DummyData" | Out-File -FilePath $fileName
 
     # Act
+    Write-Host "RUN: appcenter distribute release -g Collaborators -f $fileName --build-version 1 --mandatory --output json"
     $r1Raw = appcenter distribute release -g Collaborators -f $fileName --build-version 1 --mandatory --output json
     Write-Host "r1Raw: $r1Raw"
     $r1 = $r1Raw | ConvertFrom-Json
 
     # Assert
-    $group = appcenter distribute groups show -g Collaborators --output json | ConvertFrom-Json
+    Write-Host "RUN: appcenter distribute groups show -g Collaborators --output json"
+    $groupRaw = appcenter distribute groups show -g Collaborators --output json
+    Write-Host "groupRaw: $groupRaw"
+    $group = $groupRaw | ConvertFrom-Json
     $r2 = $group[1].tables | Where-Object {$_.id -eq $r1.id}
     $r2.mandatoryUpdate | Should -Be "True"
 
