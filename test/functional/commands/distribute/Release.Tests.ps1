84d02a507b1bec62af54b132a0417037c523b5b5
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-06-15 16:15:57 +0300

    Generate the file

--- test/functional/commands/distribute/Release.Tests.ps1
+++ test/functional/commands/distribute/Release.Tests.ps1
@@ -1,17 +1,16 @@
 Describe "distribute release" {
 
   It "creates a release when mandatory flag is set to true" {
-
     $appDisplayName = "TestDistributeRelease$((Get-Date).ToString("yyyy-MM-dd_HH.mm.ffffff"))"
     $appOs = "Custom"
     $appPlatform = "Custom"
     $app = appcenter apps create --platform $appPlatform --os $appOs --display-name $appDisplayName --output json | ConvertFrom-Json
     $appFullName = $app.owner.name + "/" + $app.name
     appcenter apps set-current $appFullName
-    
-    # Arrange
-     $fileName = "Dummy.zip"
-    "DummyData" | Out-File -FilePath $fileName
+
+    # A 10 MB file should be enough to test uploader logic.
+    $fileName = "Dummy.zip"
+    [io.file]::Create($filename).SetLength(1024 * 1024 * 10).Close
 
     # Act
     $r1 = appcenter distribute release -g Collaborators -f $fileName --build-version 1 --mandatory --output json | ConvertFrom-Json
