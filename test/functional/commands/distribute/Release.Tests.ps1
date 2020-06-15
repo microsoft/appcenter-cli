0308c2353fa728b1e446b771ddfa415e53ce8b2a
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-06-15 14:00:22 +0300

    Fix dummy file upload and deleting the app

--- test/functional/commands/distribute/Release.Tests.ps1
+++ test/functional/commands/distribute/Release.Tests.ps1
@@ -1,14 +1,17 @@
 Describe "distribute release" {
-  $appDisplayName = "TestDistributeRelease$((Get-Date).ToString("yyyy-MM-dd_HH.mm.ffffff"))"
-  $appOs = "Custom"
-  $appPlatform = "Custom"
-  $app = appcenter apps create --platform $appPlatform --os $appOs --display-name $appDisplayName --output json | ConvertFrom-Json
-  $appFullName = $app.owner.name + "/" + $app.name
-  appcenter apps set-current $appFullName
 
   It "creates a release when mandatory flag is set to true" {
+
+    $appDisplayName = "TestDistributeRelease$((Get-Date).ToString("yyyy-MM-dd_HH.mm.ffffff"))"
+    $appOs = "Custom"
+    $appPlatform = "Custom"
+    $app = appcenter apps create --platform $appPlatform --os $appOs --display-name $appDisplayName --output json | ConvertFrom-Json
+    $appFullName = $app.owner.name + "/" + $app.name
+    appcenter apps set-current $appFullName
+    
     # Arrange
-    $fileName = "./test/functional/dummyapk.apk"
+     $fileName = "Dummy.zip"
+    "DummyData" | Out-File -FilePath $fileName
 
     # Act
     $r1 = appcenter distribute release -g Collaborators -f $fileName --build-version 1 --mandatory --output json | ConvertFrom-Json
@@ -19,8 +22,10 @@ Describe "distribute release" {
     $r2.mandatoryUpdate | Should -Be "True"
 
     Remove-Item $fileName
-  }
 
-  # Cleanup
-  appcenter apps delete --app $appFullName --quiet
+    # Cleanup
+    # Note: don't put this command under the test: it ignores the async execution 
+    # of the command and the test fails.
+    appcenter apps delete --app $appFullName --quiet
+  }
 }
