256bc91642fffbc9a958ac4510ff340c852efe50
Jeroen Rietveld <jerietve@microsoft.com>
2020-08-28 10:38:34 +0900

    Run prettier

--- appcenter-file-upload-client-node/.eslintrc.js
+++ appcenter-file-upload-client-node/.eslintrc.js
@@ -7,14 +7,14 @@ module.exports = {
     jest: true,
     node: true,
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     project: path.resolve(__dirname, "./tsconfig.json"), // this can be an array if you need to reference more than one tsconfig
     tsconfigRootDir: __dirname,
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ['@typescript-eslint', 'security'],
+  plugins: ["@typescript-eslint", "security"],
   rules: {
     // Only the latest loaded 'instance' of a rule counts. If ESLint encounters a rule twice, the later one completely overrides the
     // earlier one.This includes rules with array parameters. Load order:
@@ -26,60 +26,56 @@ module.exports = {
     // undo the rule set here.
     //
     // Please keep sorted alphabetically to avoid confusion due to rules occurring multiple times.
-    '@typescript-eslint/no-array-constructor': 'error',
-    '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
-    '@typescript-eslint/prefer-for-of': 'error',
-    'curly': 'error',
-    'eqeqeq': ['error', 'always', { null: 'ignore' }],
-    'eol-last': 'error',
-    'no-caller': 'error',
-    'no-debugger': 'error',
-    'no-delete-var': 'error',
-    'no-empty': 'error',
-    'no-eval': 'error',
-    'no-new-func': 'error',
-    'no-new-wrappers': 'error',
-    'no-octal': 'error',
-    'no-octal-escape': 'error',
-    'no-redeclare': 'error',
-    'no-restricted-properties': [
+    "@typescript-eslint/no-array-constructor": "error",
+    "@typescript-eslint/no-floating-promises": ["error", { ignoreVoid: true }],
+    "@typescript-eslint/prefer-for-of": "error",
+    curly: "error",
+    eqeqeq: ["error", "always", { null: "ignore" }],
+    "eol-last": "error",
+    "no-caller": "error",
+    "no-debugger": "error",
+    "no-delete-var": "error",
+    "no-empty": "error",
+    "no-eval": "error",
+    "no-new-func": "error",
+    "no-new-wrappers": "error",
+    "no-octal": "error",
+    "no-octal-escape": "error",
+    "no-redeclare": "error",
+    "no-restricted-properties": [
       2,
       {
-        object: '_',
-        property: 'assign',
+        object: "_",
+        property: "assign",
       },
       {
-        object: '_',
-        property: 'concat',
+        object: "_",
+        property: "concat",
       },
       {
-        object: '_',
-        property: 'extend',
+        object: "_",
+        property: "extend",
       },
       {
-        object: 'Math',
-        property: 'random',
-        message:
-          'Use crypto.randomBytes() or window.crypto.getRandomValues() instead.',
+        object: "Math",
+        property: "random",
+        message: "Use crypto.randomBytes() or window.crypto.getRandomValues() instead.",
       },
     ],
-    'no-restricted-syntax': [
-      'error',
+    "no-restricted-syntax": [
+      "error",
       {
         selector: "CallExpression[callee.name='execScript']",
-        message: 'Do not use the execScript functions',
+        message: "Do not use the execScript functions",
       },
     ],
-    'no-unsafe-finally': 'error',
-    'no-unused-labels': 'error',
-    '@typescript-eslint/no-unused-expressions': [
-      'error',
-      { allowShortCircuit: false, allowTernary: true },
-    ],
-    'no-var': 'error',
-    'prefer-const': 'error',
-    'radix': 'error',
-    'security/detect-non-literal-require': 'error',
-    'security/detect-possible-timing-attacks': 'error',
+    "no-unsafe-finally": "error",
+    "no-unused-labels": "error",
+    "@typescript-eslint/no-unused-expressions": ["error", { allowShortCircuit: false, allowTernary: true }],
+    "no-var": "error",
+    "prefer-const": "error",
+    radix: "error",
+    "security/detect-non-literal-require": "error",
+    "security/detect-possible-timing-attacks": "error",
   },
-}
+};
