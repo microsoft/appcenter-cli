a19e89aae8c1439299282b60bef7fa5ee5c0fb5f
Aleksey Kharchenko <v-alkhar@microsoft.com>
2020-10-26 15:50:30 +0300

    Added test for the binary version.

--- test/commands/codepush/release-cordova-test.ts
+++ test/commands/codepush/release-cordova-test.ts
@@ -7,6 +7,7 @@ import { expect } from "chai";
 import * as which from "which";
 import * as cp from "child_process";
 import * as fs from "fs";
+import * as validationUtils from "../../../src/commands/codepush/lib/validation-utils";
 
 describe.only("Codepush release-cordova command", function () {
   const app = "bogus/app";
@@ -127,6 +128,7 @@ describe.only("Codepush release-cordova command", function () {
   it("reads the binary version from config.xml if not provided for the command", async function () {
     // Arrange
     const os = "iOS";
+    const version = "1.0.0";
     // prettier-ignore
     const args: CommandArgs = {
       ...goldenPathArgs,
@@ -143,17 +145,26 @@ describe.only("Codepush release-cordova command", function () {
       platform: "cordova",
     });
     sandbox.stub(cp, "execSync");
-    sandbox.stub(fs, "readFileSync").withArgs(Sinon.match(/.*\/config.xml/), Sinon.match.any).returns(`
-        <?xml version="1.0" encoding="utf-8"?>
-        <widget id="com.example.test" version="1.0.0" xmlns="http://www.w3.org/ns/widgets" xmlns:cdv="http://cordova.apache.org/ns/1.0">
-        </widget>
-      `);
+    const originalReadFileSync = fs.readFileSync;
+    sandbox.stub(fs, "readFileSync").callsFake(function (path: fs.PathLike) {
+      if (typeof path === "string" && /.*\/config.xml/.test(path)) {
+        return `
+          <?xml version="v${version}" encoding="utf-8"?>
+          <widget id="com.example.test" version="1.0.0" xmlns="http://www.w3.org/ns/widgets" xmlns:cdv="http://cordova.apache.org/ns/1.0">
+          </widget>
+        `;
+      } else {
+        return originalReadFileSync.apply(this, arguments);
+      }
+    });
     sandbox.stub(command, "release" as any).resolves(<CommandResult>{ succeeded: true });
+    const validSpy = sandbox.spy(validationUtils, "isValidRange");
 
     // Act
     const result = await command.execute();
     // Assert
     expect(result.succeeded).to.be.true;
+    expect(validSpy.calledOnceWith(version)).to.be.true;
   });
   it("fails the command when semver incompatible binary version specified", function () {});
   context("cli Command", function () {
