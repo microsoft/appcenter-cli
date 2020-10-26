8c0b4a0e0c1cb50977561400bf46583ca658538a
Aleksey Kharchenko <v-alkhar@microsoft.com>
2020-10-26 16:29:29 +0300

    Removed only.

--- test/commands/codepush/release-cordova-test.ts
+++ test/commands/codepush/release-cordova-test.ts
@@ -10,7 +10,7 @@ import * as fs from "fs";
 import * as validationUtils from "../../../src/commands/codepush/lib/validation-utils";
 import * as path from "path";
 
-describe.only("Codepush release-cordova command", function () {
+describe("Codepush release-cordova command", function () {
   const app = "bogus/app";
   const deployment = "bogus-deployment";
   let sandbox: Sinon.SinonSandbox;
