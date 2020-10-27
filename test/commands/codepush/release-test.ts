62e23b3422d714820be2e1ae9375dbaca5cc49e1
ruslan-bikkinin <v-rubikk@microsoft.com>
2020-10-27 13:34:52 +0300

    WIP

--- test/commands/codepush/release-test.ts
+++ test/commands/codepush/release-test.ts
@@ -28,6 +28,7 @@ describe.only("codepush release command", () => {
 
   let nockedApiGatewayRequests: Nock.Scope;
   let nockedFileUploadServiceRequests: Nock.Scope;
+  let nockedCreateReleaseInterceptor: Nock.Interceptor;
   let sandbox: Sinon.SinonSandbox;
   let stubbedSign: Sinon.SinonStub;
 
@@ -76,7 +77,7 @@ describe.only("codepush release command", () => {
         state: "Done",
       });
 
-    nockCreateRelease({ mandatory: false, disabled: false });
+    nockCreateRelease({ mandatory: false, disabled: false, statusCode: 201 });
 
     sandbox = Sinon.createSandbox();
     stubbedSign = sandbox.stub(updateContentsTasks, "sign");
@@ -84,6 +85,7 @@ describe.only("codepush release command", () => {
 
   afterEach(() => {
     Nock.cleanAll();
+    nockedCreateReleaseInterceptor = null;
     sandbox.restore();
   });
 
@@ -110,7 +112,7 @@ describe.only("codepush release command", () => {
     };
 
     nockPlatformRequest("Cordova", fakeParamsForRequests, nockedApiGatewayRequests);
-    nockCreateRelease({ mandatory: true, disabled: true });
+    nockCreateRelease({ mandatory: true, disabled: true, statusCode: 201 });
 
     // Act
     const command = new CodePushReleaseCommand(goldenArgs);
@@ -208,15 +210,76 @@ describe.only("codepush release command", () => {
       });
     });
   });
-  context("--target-binary-version correction", function () {
-    it("should correct value if not fulfilled", async function () {});
-  });
   context("edge cases after uploading release", function () {
     describe("when 409 error is returned after uploading the bundle", function () {
-      it("should fail if --disable-duplicate-release-error is set to false", async function () {});
-      it("should succeed if --disable-duplicate-release-error is set to true", async function () {});
+      it("should fail if --disable-duplicate-release-error is not set", async function () {
+        // Arrange
+        const releaseFilePath = createFile(tmpFolderPath, releaseFileName, releaseFileContent);
+        // prettier-ignore
+        const args: CommandArgs = getCommandArgsForReleaseCommand(
+        [
+          "-c", releaseFilePath,
+          "--description", appDescription,
+          "-t", fakeParamsForRequests.appVersion,
+        ],
+          fakeParamsForRequests
+        );
+
+        nockCreateRelease({ mandatory: false, disabled: false, statusCode: 409 });
+
+        // Act
+        const command = new CodePushReleaseCommand(args);
+        const result = (await command.execute()) as CommandFailedResult;
+
+        // Assert
+        expect(result.succeeded).to.be.false;
+      });
+      it("should succeed if --disable-duplicate-release-error is set", async function () {
+        // Arrange
+        const releaseFilePath = createFile(tmpFolderPath, releaseFileName, releaseFileContent);
+        // prettier-ignore
+        const args: CommandArgs = getCommandArgsForReleaseCommand(
+        [
+          "-c", releaseFilePath,
+          "--description", appDescription,
+          "-t", fakeParamsForRequests.appVersion,
+          "--disable-duplicate-release-error"
+        ],
+          fakeParamsForRequests
+        );
+
+        nockCreateRelease({ mandatory: false, disabled: false, statusCode: 409 });
+
+        // Act
+        const command = new CodePushReleaseCommand(args);
+        const result = await command.execute();
+
+        // Assert
+        expect(result.succeeded).to.be.true;
+      });
     });
-    it("should fail if 503 error is returned", async function () {});
+    it("should fail if 503 error is returned", async function () {
+      // Arrange
+      const releaseFilePath = createFile(tmpFolderPath, releaseFileName, releaseFileContent);
+      // prettier-ignore
+      const args: CommandArgs = getCommandArgsForReleaseCommand(
+      [
+        "-c", releaseFilePath,
+        "--description", appDescription,
+        "-t", fakeParamsForRequests.appVersion
+      ],
+        fakeParamsForRequests
+      );
+
+      nockCreateRelease({ mandatory: false, disabled: false, statusCode: 503 });
+
+      // Act
+      const command = new CodePushReleaseCommand(args);
+      const result = await command.execute();
+
+      // Assert
+      expect(result.succeeded).to.be.false;
+    });
     it("should remove temporary zip bundle at the end", async function () {});
   });
   context("signed release", () => {
@@ -259,34 +322,38 @@ describe.only("codepush release command", () => {
     });
   });
 
-  function nockCreateRelease(options: { mandatory: boolean; disabled: boolean }) {
-    nockedApiGatewayRequests
-      .post(
-        `/${fakeParamsForRequests.appVersion}/apps/${fakeParamsForRequests.userName}/${fakeParamsForRequests.appName}/deployments/Staging/releases`,
-        {
-          release_upload: releaseUploadResponse,
-          target_binary_version: fakeParamsForRequests.appVersion,
-          mandatory: options.mandatory,
-          disabled: options.disabled,
-          description: appDescription,
-          rollout: 100,
-        }
-      )
-      .reply(201, {
-        target_binary_range: fakeParamsForRequests.appVersion,
-        blob_url: "storagePackage.blobUrl",
-        description: "storagePackage.description",
-        is_disabled: "storagePackage.isDisabled",
-        is_mandatory: options.mandatory,
-        label: "storagePackage.label",
-        original_deployment: "storagePackage.originalDeployment",
-        original_label: "storagePackage.originalLabel",
-        package_hash: "storagePackage.packageHash",
-        released_by: "userEmail",
-        release_method: "releaseMethod",
+  function nockCreateRelease(options: { mandatory: boolean; disabled: boolean; statusCode: number }) {
+    if (nockedCreateReleaseInterceptor) {
+      Nock.removeInterceptor(nockedCreateReleaseInterceptor);
+    }
+
+    nockedCreateReleaseInterceptor = nockedApiGatewayRequests.post(
+      `/${fakeParamsForRequests.appVersion}/apps/${fakeParamsForRequests.userName}/${fakeParamsForRequests.appName}/deployments/Staging/releases`,
+      {
+        release_upload: releaseUploadResponse,
+        target_binary_version: fakeParamsForRequests.appVersion,
+        mandatory: options.mandatory,
+        disabled: options.disabled,
+        description: appDescription,
         rollout: 100,
-        size: 512,
-        upload_time: "uploadTime",
-      });
+      }
+    );
+
+    nockedCreateReleaseInterceptor.reply(options.statusCode, {
+      target_binary_range: fakeParamsForRequests.appVersion,
+      blob_url: "storagePackage.blobUrl",
+      description: "storagePackage.description",
+      is_disabled: "storagePackage.isDisabled",
+      is_mandatory: options.mandatory,
+      label: "storagePackage.label",
+      original_deployment: "storagePackage.originalDeployment",
+      original_label: "storagePackage.originalLabel",
+      package_hash: "storagePackage.packageHash",
+      released_by: "userEmail",
+      release_method: "releaseMethod",
+      rollout: 100,
+      size: 512,
+      upload_time: "uploadTime",
+    });
   }
 });
