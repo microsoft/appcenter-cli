71def7dda046afd9aae517bf3bc88938b47700a5
Dmitrii Chichuk <jamminroot@gmail.com>
2021-04-21 18:02:49 +0300

    Update logging output

--- appcenter-file-upload-client-node/src/ac-fus-util.ts
+++ appcenter-file-upload-client-node/src/ac-fus-util.ts
@@ -1,16 +1,21 @@
 import fetch from "node-fetch";
 import * as ProxyAgent from "proxy-agent";
+const debug = require("debug")("appcenter-cli:util:apis:create-client");
 
 export function resolveProxySettings(options: any = {}) {
   const instanceOptions = {
     ...options
   };
   const proxyServer = process.env.http_proxy ||
-                  process.env.HTTP_PROXY ||
-                  process.env.https_proxy ||
-                  process.env.HTTPS_PROXY ||
-                  null;
+    process.env.HTTP_PROXY ||
+    process.env.https_proxy ||
+    process.env.HTTPS_PROXY ||
+    null;
+  if (proxyServer && !proxyServer.startsWith("http://") && !proxyServer.startsWith("https://")) {
+    console.warn("Proxy address in environment variable does not start with either 'http://' or 'https://'. CLI might not work correct.");
+  }
   if (!options.agent && proxyServer) {
+    debug(`Using proxy server: ${proxyServer}`);
     instanceOptions.agent = ProxyAgent(proxyServer);
   }
   return instanceOptions;
