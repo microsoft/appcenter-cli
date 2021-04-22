6c97009f2c2674ac46932ee656462de9edd736e9
Dmitrii Chichuk <v-dmchic@microsoft.com>
2021-04-22 12:28:03 +0300

    Update logging (#1389)
    * Update logging output
    
    * Fix logging
    
    * Update appcenter-file-upload-client-node/src/ac-fus-util.ts
    
    Co-authored-by: Dima <v-dmkira@microsoft.com>
    
    * Update ac-fus-util.ts
    
    Co-authored-by: Dima <v-dmkira@microsoft.com>

--- appcenter-file-upload-client-node/src/ac-fus-util.ts
+++ appcenter-file-upload-client-node/src/ac-fus-util.ts
@@ -6,10 +6,13 @@ export function resolveProxySettings(options: any = {}) {
     ...options
   };
   const proxyServer = process.env.http_proxy ||
-                  process.env.HTTP_PROXY ||
-                  process.env.https_proxy ||
-                  process.env.HTTPS_PROXY ||
-                  null;
+    process.env.HTTP_PROXY ||
+    process.env.https_proxy ||
+    process.env.HTTPS_PROXY ||
+    null;
+  if (proxyServer && !proxyServer.startsWith("http://") && !proxyServer.startsWith("https://")) {
+    console.warn("Proxy address in environment variable does not start with either 'http://' or 'https://'. CLI might not work correctly.");
+  }
   if (!options.agent && proxyServer) {
     instanceOptions.agent = ProxyAgent(proxyServer);
   }
